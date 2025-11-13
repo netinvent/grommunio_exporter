415144a2883e3a853ddb68df5c5123f8247ad420
deajan <ozy@netpower.fr>
2025-11-13 12:04:59 +0100

    Make sure storage quotas are expressed in Bytes

--- grommunio_exporter/grommunio_api.py
+++ grommunio_exporter/grommunio_api.py
@@ -317,17 +317,17 @@ class GrommunioExporter:
                         )
                     elif key == "storagequotalimit":
                         # Value given in KB iec, we need to convert it to bytes
-                        value = BytesConverter(f"{value} KiB")
+                        value = BytesConverter(f"{value} KiB").bytes
                         self.gauge_grommunio_mailbox_storage_quota_limit.labels(
                             *labels
                         ).set(value)
                     elif key == "prohibitreceivequota":
-                        value = BytesConverter(f"{value} KiB")
+                        value = BytesConverter(f"{value} KiB").bytes
                         self.gauge_grommunio_mailbox_prohibit_reveive_quota.labels(
                             *labels
                         ).set(value)
                     elif key == "prohibitsendquota":
-                        value = BytesConverter(f"{value} KiB")
+                        value = BytesConverter(f"{value} KiB").bytes
                         self.gauge_grommunio_mailbox_prohibit_send_quota.labels(
                             *labels
                         ).set(value)
